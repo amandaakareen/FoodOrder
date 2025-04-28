a1b9385aab9dd58a8f300af41cb6027a77606d92
Amanda Karen <amandakaren@MacBook-Pro-da-Amanda-Karen.local>
2025-04-28 16:48:31 -0300

    configurando variáveis de ambiente do banco

--- internal/infra/postgres.go
+++ internal/infra/postgres.go
@@ -1,7 +1,9 @@
 package infra
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/lib/pq"
@@ -9,7 +11,14 @@ import (
 
 func Connetc() *gorm.DB {
 	var err error
-	connStr := "user=admin password=foood dbname=food sslmode=disable"
+
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+
+	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
 
 	db, err := gorm.Open("postgres", connStr)
 	if err != nil {
